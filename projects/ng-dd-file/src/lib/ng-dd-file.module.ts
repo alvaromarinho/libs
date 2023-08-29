977afc03af7774ba62a404a2a647aa4143bd1967
Alvaro Marinho <alvaromarinho@Alvaros-MacBook-Air.local>
2023-08-29 15:02:18 -0300

    new version

--- projects/ng-dd-file/src/lib/ng-dd-file.module.ts
+++ projects/ng-dd-file/src/lib/ng-dd-file.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgDdFileComponent } from './ng-dd-file.component';
 import { NgDdFileDirective } from './ng-dd-file.directive';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
