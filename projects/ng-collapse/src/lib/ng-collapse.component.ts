d9a361a4669578f68f55adec947a671b14d00dc8
Alvaro Marinho <alvaromarinho@Alvaros-MacBook-Air.local>
2023-12-13 16:14:53 -0300

    bugfix

--- projects/ng-collapse/src/lib/ng-collapse.component.ts
+++ projects/ng-collapse/src/lib/ng-collapse.component.ts
@@ -3,17 +3,29 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'ng-collapse',
     template: `
-        <div class="ng-collapse" [ngClass]="{'show': toggle}">
+        <div class="ng-collapse" [ngClass]="{'show': getToggle(), 'overflow': overflow}">
             <ng-content></ng-content>
         </div>
     `,
     styles: [`
         .ng-collapse { max-height: 0; overflow: hidden; transition: max-height 0.3s; }
         .ng-collapse.show { max-height: 100vh; transition: max-height 0.5s ease-in; }
+        .ng-collapse.show.overflow { overflow: visible }
     `]
 })
 export class NgCollapseComponent {
 
     @Input() toggle?: boolean;
+    protected overflow?: boolean;
+
+    getToggle() {
+        if (this.toggle) {
+            setTimeout(() => this.overflow = true, 500);
+        } else {
+            this.overflow = false
+        }
+
+        return this.toggle;
+    }
 
 }
